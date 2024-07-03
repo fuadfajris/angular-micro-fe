c3510117c43147eca3f133edb3db57ddbd9fac75
fuad fajri soba <fuadfajrisoba@gmail.com>
2024-07-04 04:00:07 +0700

    test agular modul federation

---
+++ projects/mfe1/src/app/mfefeature/mfefeature.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MfefeatureComponent } from './mfefeature.component';
+
+describe('MfefeatureComponent', () => {
+  let component: MfefeatureComponent;
+  let fixture: ComponentFixture<MfefeatureComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ MfefeatureComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(MfefeatureComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
